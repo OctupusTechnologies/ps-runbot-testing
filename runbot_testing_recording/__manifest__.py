da3a24064db814657d3498440887da66ab8ebea8
sergiogonzalez <sergio.gonzalez@octupus.es>
2022-02-21 15:06:57 +0100

    [migrate] to v 15

--- runbot_testing_recording/__manifest__.py
+++ runbot_testing_recording/__manifest__.py
@@ -5,7 +5,7 @@
     Register any actions set in the interface to generate automated tests by a functional teams
     """,
     'category': '',
-    'version': '0.1',
+    'version': '0.4',
 
     'depends': [
         'base',
@@ -17,12 +17,20 @@
         'views/record.xml',
         'wizards/test_start.xml',
         'wizards/caught_error.xml',
-        'templates/templates.xml',
+        # 'templates/templates.xml',
     ],
     'demo': [
     ],
     'qweb': [
-        'static/src/xml/template.xml'
+        # 'static/src/xml/template.xml'
     ],
+    'assets': {
+        'web.assets_backend': [
+            'runbot_testing_recording/static/src/js/debug.js',
+            # 'helpdesk/static/src/css/portal_helpdesk.css',
+            # 'helpdesk/static/src/js/helpdesk_dashboard.js',
+            # 'helpdesk/static/src/js/tours/helpdesk.js',
+        ]
+    },
     'auto_install' : False,
 }
